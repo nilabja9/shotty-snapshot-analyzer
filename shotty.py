57ede2b3b64d55d5f07afa1c1478cc8a34aed374
nilabja9 <Indrani#7>
2019-02-16 23:40:45 -0600

    Developed Script to automate Instance, Volume and Snapshot Listing

--- shotty.py
+++ shotty.py
@@ -15,10 +15,83 @@ def filter_instances():
 
     return instances
 
-
 @click.group()
+def cli():
+    """"Shotty manages snapshots"""
+
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for Snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None)
+def list_snapshots(project):
+    "List EC2 Snapshots"
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+
+    return
+
+
+@cli.group('volumes')
+def volumes():
+    """Commands for Volumes"""
+
+
+@volumes.command('list')
+@click.option('--project', default=None)
+def list_volumes(project):
+    "List EC2 Volumes"
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(" ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "Gib",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+
+    return
+
+
+
+@cli.group('instances')
 def instances():
     """Commands for Instances"""
+
+@instances.command('snapshot', help="Create snapshots of all volumes")
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name)")
+def create_snapshots(project):
+    "Create snapshots for EC2 Instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Snapshotalyer")
+
+    return
+
+
+
+
+
 
 @instances.command('list')
 @click.option('--project', default=None)
